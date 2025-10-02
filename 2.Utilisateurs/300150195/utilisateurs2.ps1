6bdf68f3431b387da353a339be5e5bafa3ad971b
SMAILIKH <smailikhlef@gmail.com>
2025-10-02 17:28:38 -0400

    Update README.md

---
+++ 2.Utilisateurs/300150195/utilisateurs2.ps1
@@ -0,0 +1,22 @@
+# Définir la liste des utilisateurs
+$Users = @(
+    @{ Nom = "Meziani"; Prenom = "Lytissia"; Login = "mlytissia"; OU = "Stagiaires" },
+    @{ Nom = "Zourane"; Prenom = "Amel"; Login = "amelz"; OU = "Stagiaires" },
+    @{ Nom = "Abed"; Prenom = "Ines"; Login = "inesa"; OU = "Stagiaires" },
+    @{ Nom = "Zerkani"; Prenom = "Kahina"; Login = "kzerkani"; OU = "Stagiaires" },
+    @{ Nom = "Hocine"; Prenom = "Sara"; Login = "shocine"; OU = "Stagiaires" }
+)
+
+# Créer des groupes (vides au départ)
+$Groups = @{
+    "GroupeFormation" = @()
+    "ProfesseursAD"   = @()
+}
+
+# Ajouter uniquement les utilisateurs dont l'OU = "Stagiaires"
+$Groups["GroupeFormation"] = $Users | Where-Object { $_.OU -eq "Stagiaires" }
+
+# Vérifier le contenu du groupe
+$Groups["GroupeFormation"] | ForEach-Object {
+    "$($_.Prenom) $($_.Nom) - Login: $($_.Login) - OU: $($_.OU)"
+}

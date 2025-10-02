53e7525cb703aea6383d1e3dea4e2b0ca83355a1
Mamefatim14 <Mamefatimdione@gmail.com>
2025-10-02 18:13:16 -0400

    Merge remote changes

---
+++ 300141625/ScriptsAD/utilisateurs1.ps1
@@ -0,0 +1,13 @@
+﻿# Utilisateurs1.ps1 - Création d'utilisateurs simulés
+$Users = @(
+    @{Nom="Dupont"; Prenom="Alice"; Login="adupont"; OU="Stagiaires"},
+    @{Nom="Lemoine"; Prenom="Sarah"; Login="slemoine"; OU="Stagiaires"},
+    @{Nom="Benali"; Prenom="Karim"; Login="kbenali"; OU="Stagiaires"},
+    @{Nom="Nguyen"; Prenom="Thierry"; Login="tnguyen"; OU="Stagiaires"},
+    @{Nom="Martin"; Prenom="Julie"; Login="jmartin"; OU="Stagiaires"}
+)
+
+Write-Host "=== LISTE DES UTILISATEURS ===" -ForegroundColor Green
+$Users | ForEach-Object { 
+    Write-Host "$($_.Prenom) $($_.Nom) - Login: $($_.Login) - OU: $($_.OU)" 
+}

9702d5785d3565f0786f57f2a656b35048f56a2f
zakariadjellouli <djellouli.zakaria.zd@gmail.com>
2025-10-05 00:23:37 -0400

    Ajout du fichier utilisateurs1.ps1

---
+++ 2.Utilisateurs/300150433/utilisateurs1.ps1
@@ -0,0 +1,12 @@
+# Créer une liste d'utilisateurs simulés
+$Users = @(
+    @{ Nom="Dupont";  Prenom="Alice";  Login="adupont";  OU="Stagiaires" },
+    @{ Nom="Lemoine"; Prenom="Sarah";  Login="slemoine"; OU="Stagiaires" },
+    @{ Nom="Benali";  Prenom="Karim";  Login="kbenali";  OU="Stagiaires" }
+    @{ Nom="djellouli"; Prenom="Zakaria"; Login="zdjellouli"; OU="Promo2025" },
+    @{ Nom="Hocine";  Prenom="Sara"; Login="shocine;  OU="Stagiaires" }
+)
+# Afficher les utilisateurs
+$Users | ForEach-Object { "$($.Prenom) $($.Nom) - Login: $($.Login) - OU: $($.OU)" }
+
+

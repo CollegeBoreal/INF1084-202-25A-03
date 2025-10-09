bc94ae479f17eed0e1e4d467c61d857be5fa4829
zakariadjellouli <djellouli.zakaria.zd@gmail.com>
2025-10-09 17:04:22 -0400

    Ajout des fichiers utilisateurs1 à 4.ps1

--- 2.Utilisateurs/300150433/utilisateurs1.ps1
+++ 2.Utilisateurs/300150433/utilisateurs1.ps1
@@ -2,11 +2,11 @@
 $Users = @(
     @{ Nom="Dupont";  Prenom="Alice";  Login="adupont";  OU="Stagiaires" },
     @{ Nom="Lemoine"; Prenom="Sarah";  Login="slemoine"; OU="Stagiaires" },
-    @{ Nom="Benali";  Prenom="Karim";  Login="kbenali";  OU="Stagiaires" }
-    @{ Nom="djellouli"; Prenom="Zakaria"; Login="zdjellouli"; OU="Promo2025" },
-    @{ Nom="Hocine";  Prenom="Sara"; Login="shocine;  OU="Stagiaires" }
+    @{ Nom="Benali";  Prenom="Karim";  Login="kbenali";  OU="Stagiaires" },
+    @{ Nom="Djellouli"; Prenom="Zakaria"; Login="zdjellouli"; OU="Promo2025" },
+    @{ Nom="Hocine";  Prenom="Sara"; Login="shocine";  OU="Stagiaires" }
 )
-# Afficher les utilisateurs
-$Users | ForEach-Object { "$($.Prenom) $($.Nom) - Login: $($.Login) - OU: $($.OU)" }
 
+# Afficher les utilisateurs
+$Users | ForEach-Object { "$($_.Prenom) $($_.Nom) - Login: $($_.Login) - OU: $($_.OU)" }
 	

1a9b7542492cd4b117dd2d1ad00436fb33d9f59d
Nelson William <keleko.nelson@gmail.com>
2025-10-10 11:29:03 -0400

    Update utilisateurs3.ps1

--- 4.OUs/300133071/utilisateurs3.ps1
+++ 4.OUs/300133071/utilisateurs3.ps1
@@ -1,10 +1,11 @@
 New-ADUser `
-  -Name "Siga Diarra" `
-  -GivenName "Diarra" `
-  -Surname "Siga" `
-  -SamAccountName "siga" `
-  -UserPrincipalName "siga@DC300133071-00.local" `
+  -Name "Alice Dupont" `
+  -GivenName "Alice" `
+  -Surname "Dupont" `
+  -SamAccountName "alice.dupont" `
+  -UserPrincipalName "alice.dupont@DC300133071-00.local" `
   -Path "CN=Users,DC=DC300133071-00,DC=local" `
   -AccountPassword (Read-Host -AsSecureString "Entrer le mot de passe") `
   -Enabled $true `
   -Credential $cred
+

4f5daf4ebfbabd65abb24f149fe391950236ffd2
unknown <Administrator@DC300133071-00.LOCAL>
2025-10-10 09:02:46 -0400

    Add

---
+++ 4.OUs/300133071/utilisateurs3.ps1
@@ -0,0 +1,9 @@
+New-ADUser `
+  -Name "Siga Diarra" `
+  -GivenName "Diarra" `
+  -Surname "Siga" `
+  -SamAccountName "siga" `
+  -UserPrincipalName "siga@DC300133071-00.local" `
+  -Path "CN=Users,DC=DC300133071-00,DC=local" `
+  -AccountPassword (Read-Host -AsSecureString "Entrer le mot de passe") `
+  -Enabled $true

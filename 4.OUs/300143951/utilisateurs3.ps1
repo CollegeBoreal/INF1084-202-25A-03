21fdb41530a038da6f1d02a5f030881ec6c4da15
frank17art <frankkadji13@gmail.com>
2025-10-25 13:14:20 -0400

    update

--- 4.OUs/300143951/utilisateurs3.ps1
+++ 4.OUs/300143951/utilisateurs3.ps1
@@ -1,14 +1,10 @@
-$domainName = "inf1084.local"
-$netbiosName = "inf1084"
-$cred = Get-Credential
-
-New-ADUser -Name "Alice Dupont" `
-    -GivenName "Alice" `
-    -Surname "Dupont" `
-    -SamAccountName "alice.dupont" `
-    -UserPrincipalName "alice.dupont@$domainName" `
-    -AccountPassword (ConvertTo-SecureString "MotDePasse123!" -AsPlainText -Force) `
-    -Enabled $true `
-    -Path "CN=Users,DC=$netbiosName,DC=local" `
-    -Credential $cred
-
+New-ADUser `
+  -Name "kadji" `
+  -GivenName "Falcon" `
+  -Surname "K falcon" `
+  -SamAccountName "Kadji.falcon" `
+  -UserPrincipalName "kadji.falcon@DC300143951-00.local" `
+  -Path "CN=Users,DC=DC300143951-00,DC=local" `
+  -AccountPassword (Read-Host -Prompt "Entrez le mot de passe pour K falcon" -AsSecureString) `
+  -Enabled $true `
+  -Credential $cred

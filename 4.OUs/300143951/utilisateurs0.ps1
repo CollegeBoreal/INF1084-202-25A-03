ee521f7e6543b60b4eb017a27a900311c15ad7ba
frank_kadji71 <frankkadji13@gmail.com>
2025-10-25 11:09:51 -0400

    Fix studentInstance variable initialization

--- 4.OUs/300143951/utilisateurs0.ps1
+++ 4.OUs/300143951/utilisateurs0.ps1
@@ -1,5 +1,5 @@
 $studentNumber = 300143951 
-$studentInstance = 0   
+$studentInstance = 00
 
 $domainName = "DC$studentNumber-$studentInstance.local"
 $netbiosName = "DC$studentNumber-$studentInstance"

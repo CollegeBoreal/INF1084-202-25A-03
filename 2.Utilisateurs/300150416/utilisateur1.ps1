616a654672e49c81c5df2142bcf66f5f55139f81
Hachem Souyadi <hachemdsq@gmail.com>
2025-10-27 13:30:09 -0400

    creation de user1

--- 2.Utilisateurs/300150416/utilisateur1.ps1
+++ 2.Utilisateurs/300150416/utilisateur1.ps1
@@ -1,17 +1,11 @@
-# Création d'utilisateurs simulés (PSCustomObject pour un accès .Propriété)
+# Créer une liste d'utilisateurs simulés
 $Users = @(
-    [pscustomobject]@{Nom="Dupont";  Prenom="Alice";  Login="adupont";  OU="Stagiaires"},
-    [pscustomobject]@{Nom="Lemoine"; Prenom="Sarah";  Login="slemoine"; OU="Stagiaires"},
-    [pscustomobject]@{Nom="Benali";  Prenom="Karim";  Login="kbenali";  OU="Stagiaires"}
+    @{Nom="Dupont"; Prenom="Alice"; Login="adupont"; OU="Stagiaires"},
+    @{Nom="Lemoine"; Prenom="Sarah"; Login="slemoine"; OU="Stagiaires"},
+    @{Nom="Benali"; Prenom="Karim"; Login="kbenali"; OU="Stagiaires"},
+    @{Nom="Benali"; Prenom="Karim"; Login="kbenali"; OU="Stagiaires"},
+    @{Nom="Benali"; Prenom="Karim"; Login="kbenali"; OU="Stagiaires"}
 )
 
-# Exercice 1 : ajouter 2 nouveaux utilisateurs
-$Users += @(
-    [pscustomobject]@{Nom="Martin"; Prenom="Nora";   Login="nmartin";  OU="Stagiaires"},
-    [pscustomobject]@{Nom="Boucher"; Prenom="Adam";  Login="aboucher"; OU="Formation"}
-)
-
-# Afficher la liste
-$Users | ForEach-Object {
-    "$($_.Prenom) $($_.Nom) - Login: $($_.Login) - OU: $($_.OU)"
-}
+# Afficher les utilisateurs
+$Users | ForEach-Object { "$($_.Prenom) $($_.Nom) - Login: $($_.Login) - OU: $($_.OU)" }

75b1326eb27e48c95345245567e70de38deb9cb4
emeraude <emeraudesantu05@gmail.com>
2025-11-06 22:59:22 -0500

    2e commit

--- 4.OUs/300141550/utilisateurs2.ps1
+++ 4.OUs/300141550/utilisateurs2.ps1
@@ -1,2 +1,2 @@
-GetADUser -Filter * -Server "DC300141550.local" -Properties Name, SamAccountName, Enabled | Where-Object { $_.Enabled -eq $true -and $_.SamAccountName -notin @("Administrator","Guest","krbtgt") } |
+Get-ADUser -Filter * -Server "DC300141550.local" -Properties Name, SamAccountName, Enabled | Where-Object { $_.Enabled -eq $true -and $_.SamAccountName -notin @("Administrator","Guest","krbtgt") } |
 Select-Object Name, SamAccountName

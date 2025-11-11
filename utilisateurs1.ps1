db18f49f8d5b2413279dcf0342042884f9640988
Hanane <Hananehenouna23@gmail.com>
2025-11-11 15:37:14 -0500

    Mise a jour de script: utilisateurs1.ps1 et utilisateurs1.ps3

--- utilisateurs1.ps1
+++ utilisateurs1.ps1
@@ -3,9 +3,9 @@ $Users = @(
 [PSCustomObject]@{Nom="Dupont"; Prenom="Alice"; Login="adupont"; OU="Stagiaires"},
 [PSCustomObject]@{Nom="Lemoine"; Prenom="Sarah"; Login="slemoine"; OU="Stagiaires"},
 [PSCustomObject]@{Nom="Benali"; Prenom="Karim"; Login="kbenali"; OU="Stagiaires"},
-[PSCustomObject]@{Nom="Zerrouki"; Prenom="Hanane"; Login="Hzerrouki"; OU="Stagiaire"},
+[PSCustomObject]@{Nom="Zerrouki"; Prenom="Hanane"; Login="Hzerrouki"; OU="Stagiaires"},
  
-[PSCustomObject]@{Nom="Zerrouki"; Prenom="Wahiba"; Login="Wzerrouki"; OU="Stagiaire"}
+[PSCustomObject]@{Nom="Zerrouki"; Prenom="Wahiba"; Login="Wzerrouki"; OU="Stagiaires"}
 )
 
 # Afficher les utilisateurs

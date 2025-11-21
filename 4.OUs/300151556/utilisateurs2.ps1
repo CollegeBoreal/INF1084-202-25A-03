e345776e2dfbcd67d86239cbc946fe20ddbf1ee9
unknown <Administrator@DC300151556-00.LOCAL>
2025-11-20 20:11:20 -0500

    Ajout utilisateurs2.ps1 - Liste utilisateurs actifs - Kahina

--- 4.OUs/300151556/utilisateurs2.ps1
+++ 4.OUs/300151556/utilisateurs2.ps1
@@ -1,3 +1,5 @@
-Get-ADUser -Filter * -Server "300151556.local" -Properties Name, SamAccountName, Enabled |
-Where-Object { $_.Enabled -eq $true -and $_.SamAccountName -notin @("Administrator","Guest","krbtgt") } |
-Select-Object Name, SamAccountName
+Import-Module ActiveDirectory
+
+Get-ADUser -Filter * -Server "DC300151556-00.local" -Properties Name, SamAccountName, Enabled `
+| Where-Object { $_.Enabled -eq $true -and $_.SamAccountName -notin @("Administrator","Guest","krbtgt") } `
+| Select-Object Name, SamAccountName

397e6cdd34ea665818bed5163ad3f97f3b4c58e7
unknown <Administrator@DC300151556-00.LOCAL>
2025-11-20 20:01:32 -0500

    Ajout du script utilisateurs2.ps1 - Infos AD - Kahina

---
+++ 4.OUs/300151556/utilisateurs2.ps1
@@ -0,0 +1,3 @@
+Get-ADUser -Filter * -Server "300151556.local" -Properties Name, SamAccountName, Enabled |
+Where-Object { $_.Enabled -eq $true -and $_.SamAccountName -notin @("Administrator","Guest","krbtgt") } |
+Select-Object Name, SamAccountName

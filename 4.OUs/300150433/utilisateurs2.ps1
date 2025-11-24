6a959c740f43bd60281d4744c39562c9878ae461
Zakariadjellouli <djellouli.zakaria.zd@gmail.com>
2025-11-24 14:50:12 -0500

    Correction des scrpits de djellouli zakaria

--- 4.OUs/300150433/utilisateurs2.ps1
+++ 4.OUs/300150433/utilisateurs2.ps1
@@ -1,3 +1,3 @@
-Get-ADUser -Filter * -Server "DC300150433-50.local" -Properties Name, SamAccountName, Enabled |
+Get-ADUser -Filter * -Server "dc300150433-00.local" -Properties Name, SamAccountName, Enabled |
 Where-Object { $_.Enabled -eq $true -and $_.SamAccountName -notin @("Administrator","Guest","krbtgt") } |
 Select-Object Name, SamAccountName

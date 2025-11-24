1652628449d265f6b9cb3ea1aa0b102faca99dc9
Zakariadjellouli <djellouli.zakaria.zd@gmail.com>
2025-11-24 14:59:01 -0500

    correction de script

--- 4.OUs/300150433/utilisateurs5.ps1
+++ 4.OUs/300150433/utilisateurs5.ps1
@@ -1,18 +1,19 @@
-# Désactiver l'utilisateur
+# Définir le nom du domaine Active Directory
+$domainName = "dc300150433-00.local"
+
+# Désactiver un utilisateur
 Disable-ADAccount -Identity "alice.dupont" -Server $domainName
 
-# Réactiver l'utilisateur
+# Activer un utilisateur
 Enable-ADAccount -Identity "alice.dupont" -Server $domainName
 
-# Supprimer l'utilisateur
+# Supprimer un utilisateur
 Remove-ADUser -Identity "alice.dupont" -Server $domainName -Confirm:$false
 
 # Rechercher les utilisateurs dont le nom commence par 'a'
 Get-ADUser -Filter "Name -like 'a*'" -Server $domainName -Properties Name, SamAccountName |
-Select-Object Name, SamAccountName
+    Select-Object Name, SamAccountName
 
-# Exporter tous les utilisateurs dans un fichier CSV
-Get-ADUser -Filter * -Server $domainName -Properties Name, SamAccountName, EmailAddress, Enabled |
-Where-Object { $_.SamAccountName -notin @("Administrator","Guest","krbtgt") } |
-Select-Object Name, SamAccountName, EmailAddress, Enabled |
-Export-Csv -Path "TP_AD_Users.csv" -NoTypeInformation -Encoding UTF8
+# Lister tous les utilisateurs
+Get-ADUser -Filter * -Server $domainName -Properties Name, SamAccountName |
+    Select-Object Name, SamAccountName

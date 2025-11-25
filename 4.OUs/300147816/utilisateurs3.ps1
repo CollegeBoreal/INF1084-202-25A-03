7cde8c4ef2c9d021a3e0ace9780c3d137d91f30f
Hanane <Hananehenouna23@gmail.com>
2025-11-25 11:02:15 -0500

    fin

--- 4.OUs/300147816/utilisateurs3.ps1
+++ 4.OUs/300147816/utilisateurs3.ps1
@@ -1,11 +1,9 @@
-
-Import-Module ActiveDirectory
 New-ADUser -Name "Alice Dupont" `
            -GivenName "Alice" `
            -Surname "Dupont" `
            -SamAccountName "adupont" `
-           -UserPrincipalName "adupont@domaine.local" `
-           -Path "OU=Utilisateurs,DC=domaine,DC=local" `
+           -UserPrincipalName "adupont@DC300147816-0.local" `
+           -Path "OU=Utilisateurs,DC=DC300147816-0,DC=local" `
            -AccountPassword (ConvertTo-SecureString "MotDePasse123!" -AsPlainText -Force) `
            -Enabled $true `
           

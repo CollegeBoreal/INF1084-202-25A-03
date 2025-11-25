811179b1961cabf66001f19c60e9d6d28e58033a
unknown <Administrator@DC300147816.LOCAL>
2025-11-25 11:35:34 -0500

    fin

--- utilisateurs3.ps1
+++ utilisateurs3.ps1
@@ -2,8 +2,8 @@ New-ADUser -Name "Alice Dupont" `
            -GivenName "Alice" `
            -Surname "Dupont" `
            -SamAccountName "adupont" `
-           -UserPrincipalName "adupont@domaine.local" `
-           -Path "OU=Utilisateurs,DC=domaine,DC=local" `
+           -UserPrincipalName "adupont@DC300147816-0.local" `
+           -Path "OU=Utilisateurs,DC=DC300147816-0,DC=local" `
            -AccountPassword (ConvertTo-SecureString "MotDePasse123!" -AsPlainText -Force) `
            -Enabled $true `
            -Credential $cred

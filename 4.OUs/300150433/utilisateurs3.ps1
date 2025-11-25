7a1d477ce2bd78933bd1675dbb62bef931908993
Zakariadjellouli <djellouli.zakaria.zd@gmail.com>
2025-11-25 10:01:38 -0500

    Correction du domaine AD dans utilisateurs3.ps1

--- 4.OUs/300150433/utilisateurs3.ps1
+++ 4.OUs/300150433/utilisateurs3.ps1
@@ -2,7 +2,7 @@ New-ADUser -Name "Alice Dupont" `
   -GivenName "Alice" `
   -Surname "Dupont" `
   -SamAccountName "alice.dupont" `
-  -UserPrincipalName "alice.dupont@DC300150433-50.local" `
+  -UserPrincipalName "alice.dupont@dc300150433-00.local" `
   -AccountPassword (ConvertTo-SecureString "MotDePasse123!" -AsPlainText -Force) `
   -Enabled $true `
-  -Path "CN=Users,DC=DC300150433-50,DC=local"
+  -Path "CN=Users,DC=dc300150433-00,DC=local"

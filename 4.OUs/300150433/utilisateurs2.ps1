61bd8cef0a6fcb913227df1f41fac112a754f9b3
Zakariadjellouli <djellouli.zakaria.zd@gmail.com>
2025-11-25 10:00:57 -0500

    Correction du domaine AD dans utilisateurs2.ps1

--- 4.OUs/300150433/utilisateurs2.ps1
+++ 4.OUs/300150433/utilisateurs2.ps1
@@ -1,3 +1,5 @@
-Get-ADUser -Filter * -Server "DC300150433-50.local" -Properties Name, SamAccountName, Enabled |
+$domainName = "dc300150433-00.local"
+
+Get-ADUser -Filter * -Server $domainName -Properties Name, SamAccountName, Enabled |
 Where-Object { $_.Enabled -eq $true -and $_.SamAccountName -notin @("Administrator","Guest","krbtgt") } |
 Select-Object Name, SamAccountName

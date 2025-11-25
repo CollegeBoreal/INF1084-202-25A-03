9dd5cc28f5993f772b9dbc33842b839e6a8dc31f
unknown <Administrator@DC300147816.LOCAL>
2025-11-25 15:05:29 -0500

    merci

--- utilisateurs9.ps1
+++ utilisateurs9.ps1
@@ -1,4 +1,4 @@
 Get-ADUser -Filter * -Server "DC300147816.local" -Properties Name, SamAccountName, EmailAddress, Enabled |
 Where-Object { $_.SamAccountName -notin @("Administrator","Guest","krbtgt") } |
 Select-Object Name, SamAccountName, EmailAddress, Enabled |
-Export-Csv -Path "TP_AD_Users.csv" -NoTypeInformation -Encoding UTF8
+Export-Csv -Path "C:\Users\Student1\INF1084-202-25A-03\4.OUs\300147816\TP_AD_Users.csv" -NoTypeInformation -Encoding UTF8

bb2b94eab5cad283371054db3814ada556f8d445
unknown <Administrator@DC300147816.LOCAL>
2025-11-25 11:46:35 -0500

    fin

--- utilisateurs3.ps1
+++ utilisateurs3.ps1
@@ -3,7 +3,7 @@ New-ADUser -Name "Alice Dupont" `
            -Surname "Dupont" `
            -SamAccountName "adupont" `
            -UserPrincipalName "adupont@DC300147816-0.local" `
-           -Path "OU=Utilisateurs,DC=DC300147816-0,DC=local" `
+           -Path "CN=Users,DC=DC300147816-0,DC=local" `
            -AccountPassword (ConvertTo-SecureString "MotDePasse123!" -AsPlainText -Force) `
            -Enabled $true `
            

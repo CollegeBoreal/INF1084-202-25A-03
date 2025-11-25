acf85786966a2c600ac965f6f7cb0a6a7fb38b4d
Zakariadjellouli <djellouli.zakaria.zd@gmail.com>
2025-11-25 10:02:32 -0500

    Correction du domaine AD dans utilisateurs5.ps1

--- 4.OUs/300150433/utilisateurs5.ps1
+++ 4.OUs/300150433/utilisateurs5.ps1
@@ -1,18 +1,18 @@
-# Désactiver l'utilisateur
+$domainName = "dc300150433-00.local"
+
+# Désactiver Alice
 Disable-ADAccount -Identity "alice.dupont" -Server $domainName
 
-# Réactiver l'utilisateur
+# Activer Alice
 Enable-ADAccount -Identity "alice.dupont" -Server $domainName
 
-# Supprimer l'utilisateur
+# Supprimer Alice
 Remove-ADUser -Identity "alice.dupont" -Server $domainName -Confirm:$false
 
-# Rechercher les utilisateurs dont le nom commence par 'a'
+# Rechercher les utilisateurs commençant par 'a'
 Get-ADUser -Filter "Name -like 'a*'" -Server $domainName -Properties Name, SamAccountName |
 Select-Object Name, SamAccountName
 
-# Exporter tous les utilisateurs dans un fichier CSV
-Get-ADUser -Filter * -Server $domainName -Properties Name, SamAccountName, EmailAddress, Enabled |
-Where-Object { $_.SamAccountName -notin @("Administrator","Guest","krbtgt") } |
-Select-Object Name, SamAccountName, EmailAddress, Enabled |
-Export-Csv -Path "TP_AD_Users.csv" -NoTypeInformation -Encoding UTF8
+# Lister tous les utilisateurs
+Get-ADUser -Filter * -Server $domainName -Properties Name, SamAccountName |
+Select-Object Name, SamAccountName

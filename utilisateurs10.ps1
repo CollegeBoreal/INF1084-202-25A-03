11d5de0c293e27ed22910549ba82474610e40a7b
unknown <Administrator@DC300147816.LOCAL>
2025-11-26 16:38:34 -0500

    merci de finir

--- utilisateurs10.ps1
+++ utilisateurs10.ps1
@@ -1,10 +1,10 @@
 # Vérifier si l'OU existe
 if (-not (Get-ADOrganizationalUnit -Filter "Name -eq 'Students'")) {
-    New-ADOrganizationalUnit -Name "Students" -Path "DC=$DC300147816,DC=local"
+    New-ADOrganizationalUnit -Name "Students" -Path "DC=DC300147816,DC=local"
 }
 
-Move-ADObject -Identity "CN=Alice Dupont,CN=Users,DC=$DC300147816,DC=local" `
-              -TargetPath "OU=Students,DC=$DC300147816,DC=local" `
+Move-ADObject -Identity "CN=Alice Dupont,CN=Users,DC=DC300147816,DC=local" `
+              -TargetPath "OU=Students,DC=DC300147816,DC=local" `
               -Credential $cred
 
 Get-ADUser -Identity "alice.dupont" | Select-Object Name, DistinguishedName

3165ab4351ceefd6bd8684861dd176fda414e6d8
unknown <Administrator@DC300150195-00.LOCAL>
2025-11-27 19:20:18 -0500

    Ajout des scripts services AD

--- 5.Services/300150195/service1.ps1
+++
@@ -1,8 +0,0 @@
-# Lister tous les services liés à AD
-Get-Service | Where-Object {
-    $_.DisplayName -like "*Directory*" -or $_.Name -match "NTDS|ADWS|DFSR|kdc|Netlogon|IsmServ"
-} | Sort-Object DisplayName
-
-# Vérifier le statut des services AD principaux
-Get-Service -Name NTDS, ADWS, DFSR
-

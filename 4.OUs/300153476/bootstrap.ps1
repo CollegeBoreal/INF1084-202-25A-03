894c6bd1d2c40dd9ad0e854e68b85a816c6d212e
Ramatoulaye Diallo <129418622+dialloramatoulayebah@users.noreply.github.com>
2025-12-04 17:11:29 -0500

    Update PSCredential domain name in bootstrap.ps1

--- 4.OUs/300153476/bootstrap.ps1
+++ 4.OUs/300153476/bootstrap.ps1
@@ -9,4 +9,7 @@ $netbiosName = "DC$studentNumber-$studentInstance"
 # les informations de sécurité
 $plain = 'Infra@2024'
 $secure = ConvertTo-SecureString $plain -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential("Administrator@$domainName", $secure)
+$cred = New-Object System.Management.Automation.PSCredential("Administrator@ramatoulaye.me", $secure)
+
+
+

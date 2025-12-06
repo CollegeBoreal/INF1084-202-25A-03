ed214d3af783f8b3138613a0141f9177cb293cde
Freedy Ebah <freedy.ebah@yahoo.com>
2025-12-06 14:46:15 -0500

    Delete 7.DCs/300151825-300153476/utilisateurs5.ps1

--- 7.DCs/300151825-300153476/utilisateurs5.ps1
+++
@@ -1,8 +0,0 @@
-# Créer un PSDrive pour parcourir AD2
-New-PSDrive -Name AD2 -PSProvider ActiveDirectory -Root "DC300153476-00.local" -Credential $credAD2
-
-# Se placer dans la racine du domaine AD2
-Set-Location AD2:\DC=DC300153476-00,DC=local
-
-# Lister les éléments du domaine AD2
-Get-ChildItem

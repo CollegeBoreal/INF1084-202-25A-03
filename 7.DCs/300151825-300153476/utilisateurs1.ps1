d1cc4878d187121f9c054c88e2983439181e9c16
Freedy Ebah <freedy.ebah@yahoo.com>
2025-12-06 14:44:53 -0500

    Delete 7.DCs/300151825-300153476/utilisateurs1.ps1

--- 7.DCs/300151825-300153476/utilisateurs1.ps1
+++
@@ -1,2 +0,0 @@
-# Demande des identifiants administrateur du domaine DC300153476-00.local
-$credAD2 = Get-Credential -Message "Entrez les identifiants administrateur du domaine DC300153476-00.local"

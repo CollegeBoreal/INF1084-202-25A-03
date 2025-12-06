45bf212e72b3a2bfbb8a6e5bebed74147342ee5c
Freedy Ebah <freedy.ebah@yahoo.com>
2025-12-06 14:45:05 -0500

    Delete 7.DCs/300151825-300153476/utilisateurs7.ps1

--- 7.DCs/300151825-300153476/utilisateurs7.ps1
+++
@@ -1,8 +0,0 @@
-# Vérifier le trust
-netdom trust DC300151825-00.local /Domain:DC300153476-00.local /Verify
-
-# Vérifier DNS
-Resolve-DnsName DC300153476-00.local
-
-# Vérifier les trusts via NLTEST
-nltest /domain_trusts

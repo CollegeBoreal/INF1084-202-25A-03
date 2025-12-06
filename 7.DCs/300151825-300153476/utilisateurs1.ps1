9f8c7cf60fcb2b6c5e3449c0eac78812190be0e1
Freedy Ebah <freedy.ebah@yahoo.com>
2025-12-06 14:24:25 -0500

    Utilisateurs1

---
+++ 7.DCs/300151825-300153476/utilisateurs1.ps1
@@ -0,0 +1,2 @@
+# Demande des identifiants administrateur du domaine DC300153476-00.local
+$credAD2 = Get-Credential -Message "Entrez les identifiants administrateur du domaine DC300153476-00.local"

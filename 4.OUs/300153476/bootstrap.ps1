d41331321f17720076825ae5e3c9ee3c4f534846
Ramatoulaye Diallo <129418622+dialloramatoulayebah@users.noreply.github.com>
2025-12-09 18:18:21 -0500

    Update bootstrap.ps1

--- 4.OUs/300153476/bootstrap.ps1
+++ 4.OUs/300153476/bootstrap.ps1
@@ -14,5 +14,3 @@ $plain = 'Infra@2024'
 $secure = ConvertTo-SecureString $plain -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential("Administrator@$domainName", $secure)
 
-# Import module Active Directory
-Import-Module ActiveDirectory

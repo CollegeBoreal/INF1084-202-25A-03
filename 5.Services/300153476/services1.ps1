93fa637c0fb9cd9905aece541ac1bac749618fda
Ramatoulaye Diallo <129418622+dialloramatoulayebah@users.noreply.github.com>
2025-12-10 15:58:07 -0500

    Rectification services

--- 5.Services/300153476/services1.ps1
+++ 5.Services/300153476/services1.ps1
@@ -3,5 +3,5 @@ Get-Service | Where-Object {
     $_.DisplayName -like "*Directory*" -or $_.Name -match "NTDS|ADWS|DFSR|kdc|Netlogon|IsmServ"
 } | Sort-Object DisplayName
 
-# Vérifier l’état d’un service spécifique
+# Vérifier e statut d'un service spécifique
 Get-Service -Name NTDS, ADWS, DFSR

febc05238c1d14935f6fd6605b3131cc3b3ba370
Leandre00126 <leandre00126@gmail.com>
2025-12-10 23:24:55 -0500

    Update utilisateurs1.ps1

--- 4.OUs/300141657/utilisateurs1.ps1
+++ 4.OUs/300141657/utilisateurs1.ps1
@@ -5,5 +5,5 @@ New-ADUser -Name "Alice Dupont" `
            -UserPrincipalName "alice.dupont@$domainName" `
            -AccountPassword (ConvertTo-SecureString "MotDePasse123!" -AsPlainText -Force) `
            -Enabled $true `
-           -Path "CN=Users,DC=DC300098957-40,DC=local" `
+           -Path "CN=Users,DC=DC300141657-00,DC=local" `
            -Credential $cred

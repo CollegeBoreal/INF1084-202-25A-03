1fefe993795930fd0c95cab8caa4765c63d5bf64
Smail Ikhlef <smail@gmail.com>
2025-12-10 23:38:17 -0500

    utilisateur 4

--- 4.OUs/300146721/utilisateurs4.ps1
+++ 4.OUs/300146721/utilisateurs4.ps1
@@ -3,25 +3,15 @@
 # Domaine: DC300146721_001.local
 # ================================
 
-# Identité étudiante
-$studentNumber   = 300146721
-$studentInstance = '001'      
+# Vérifier si l'OU "Students" existe, sinon la créer
+if (-not (Get-ADOrganizationalUnit -Filter "Name -eq 'Students'" -Server $domainName -ErrorAction SilentlyContinue)) {
+    New-ADOrganizationalUnit -Name "Students" -Path "DC=$netbiosName,DC=local" -Server $domainName
+}
 
-# Désactivation du compte
-Disable-ADAccount -Identity "alice.dupont" -Credential $cred
+# Déplacer l'utilisateur "Alice Dupont" vers l'OU "Students"
+Move-ADObject -Identity "CN=Alice Dupont,CN=Users,DC=$netbiosName,DC=local" `
+              -TargetPath "OU=Students,DC=$netbiosName,DC=local" `
+              -Server $domainName
 
-# Réactivation du compte
-Enable-ADAccount -Identity "alice.dupont" -Credential $cred
-
-# Suppression du compte
-Remove-ADUser -Identity "alice.dupont" -Credential $cred -Confirm:$false
-
-# Liste des utilisateurs dont le prénom commence par "A"
-Get-ADUser -Filter "GivenName -like 'A*'" -Server "DC300146721-001.local" -Properties Name, SamAccountName |
-Select-Object Name, SamAccountName
-
-# Exportation de la liste complète des utilisateurs dans un fichier CSV
-Get-ADUser -Filter * -Server "DC300146721-00.local" -Properties Name, SamAccountName, EmailAddress, Enabled |
-Where-Object { $_.SamAccountName -notin @("Administrator","Guest","krbtgt") } |
-Select-Object Name, SamAccountName, EmailAddress, Enabled |
-Export-Csv -Path "TP_AD_Users.csv" -NoTypeInformation -Encoding UTF8
+# Vérifier le déplacement
+Get-ADUser -Identity "alice.dupont" -Server $domainName | Select-Object Name, DistinguishedName

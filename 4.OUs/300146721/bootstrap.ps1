f250d662ac282fdb0f2f2bbb3f65010b4787aeb4
SMAIL IKHLEF <smailikhlef@gmail.com>
2025-12-10 23:02:37 -0500

    Update bootstrap.ps1

--- 4.OUs/300146721/bootstrap.ps1
+++ 4.OUs/300146721/bootstrap.ps1
@@ -15,17 +15,4 @@ $netbiosName = "DC${studentNumber}_${studentInstance}"
 $plain  = 'Infra@2024'
 $secure = ConvertTo-SecureString $plain -AsPlainText -Force
 
-# Credential admin du domaine
-$cred = New-Object System.Management.Automation.PSCredential("Administrator@$domainName", $secure)
 
-# Module AD et vérifications
-Import-Module ActiveDirectory
-
-# Vérifier que le domaine et le DC répondent
-Get-ADDomain -Server $domainName
-Get-ADDomainController -Filter * -Server $domainName | Select-Object HostName,IPv4Address,Site,IsGlobalCatalog
-
-# S'assurer que l'OU Students existe
-if (-not (Get-ADOrganizationalUnit -LDAPFilter '(ou=Students)' -Server $domainName -ErrorAction SilentlyContinue)) {
-    New-ADOrganizationalUnit -Name "Students" -Path "DC=$netbiosName,DC=local" -Server $domainName -ProtectedFromAccidentalDeletion:$false
-}
